48cbc30660b7aca54a2026ca241c13651bc853dc
Admiral Ion <infinites999@gmail.com>
2022-08-26 23:31:11 +0530

    working on Sidebar E 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import react from 'react';
 import {useSelector} from 'react-redux';
 import Login from './Views/Login/login'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './Views/Sidebar/sidebar'
+
 
 function App() {
   const {auth} = useSelector(state => state)
   return (
-    <>
+    <Router>
     {!auth.LoggedIn ? <Login /> :
-    <></>
+    <>
+    <Sidebar />
+    </>
     }
-    </> 
+    </Router> 
   );
 }
 
---
+++ src/Views/Sidebar/sidebar.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { useDispatch,useSelector } from 'react-redux'
+
+import {signOut} from '../../Controllers/Redux/authSlice'
+import './sidebar.css'
+
+
+export default () => {
+    
+    
+    const dispatch = useDispatch();
+    const {auth} = useSelector(state => state)
+    
+    function SignOut(){
+        dispatch(signOut());
+    }
+    
+    return (
+      <div className="sidebar">
+          <Link className='Nav-Link' to="/">
+              <h1 className="brand">Bug-tracker</h1>
+          </Link>
+          <ul>
+              <li><Link to="/" className="nav-link">Dashboard</Link></li>
+              <li><Link to="/viewbugs" className="nav-link">View Bugs</Link></li>
+              <li>{auth.Admin && <Link to="/createbugs" className="nav-link">Create Bug</Link> }</li>
+          </ul>
+          <button className="nav-link logout" onClick={signOut} >Logout</button>
+      </div>
+    )
+
+}
